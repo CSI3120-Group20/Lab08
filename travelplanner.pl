a857770cadd863beef68e56eaf15f5fb6ef35ea4
jordanxlau <jordanxlau@yahoo.ca>
2024-11-19 20:20:13 -0500

    Initial Commit

---
+++ travelplanner.pl
@@ -0,0 +1,11 @@
+% 1. Destination Management
+
+% 2. Expense Tracking
+
+% 3. Budget Validation
+
+% 4. Filtering Destinations and Expenses
+
+% 5. Command Parsing with DCGs
+
+% 6. Saving and Loading Journey (File I/O)
